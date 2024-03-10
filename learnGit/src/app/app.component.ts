1a8f4818e2f80b0f8e4414c0988e791882a7f93a
AvitalSt <avitalsty@gmail.com>
2024-03-10 17:34:15 +0200

    adding true/false radio button

--- learnGit/src/app/app.component.ts
+++ learnGit/src/app/app.component.ts
@@ -1,30 +1,41 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 import Users from './user.model';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+  public isShown = false;
+  public selectedOption = true
   public users: Users[] = [
     {
-      firstName: "teacher", 
+      firstName: "teacher",
       lastName: "teacher",
       isLikeAngular: true
     },
     {
-      firstName: "Rachel", 
+      firstName: "Rachel",
       lastName: "Weiss",
       isLikeAngular: true
     },
     {
-      firstName: "Racheli", 
+      firstName: "Racheli",
       lastName: "Margalit",
       isLikeAngular: true
     }
   ]
+  public showList() {
+    if (this.selectedOption== true){
+      this.isShown = this.selectedOption;
+    }
+    else{
+      this.isShown= false;
+    }
+  }
 }
