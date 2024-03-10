8b4e6b5bfb9d09b406db64bd8d53bf9e407bdc3a
racheli1203 <rachelim1203@gmail.com>
2024-03-10 17:47:01 +0200

    search field

--- learnGit/src/app/app.component.ts
+++ learnGit/src/app/app.component.ts
@@ -1,30 +1,46 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import Users from './user.model';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
+
 export class AppComponent {
+  public filterArray!: Users[];
+  public searchInput: string = "";
+
+  constructor() {
+    this.filterArray = this.users;
+  }
+
   public users: Users[] = [
     {
-      firstName: "teacher", 
+      firstName: "teacher",
       lastName: "teacher",
       isLikeAngular: true
     },
     {
-      firstName: "Rachel", 
+      firstName: "Rachel",
       lastName: "Weiss",
       isLikeAngular: true
     },
     {
-      firstName: "Racheli", 
+      firstName: "Racheli",
       lastName: "Margalit",
       isLikeAngular: true
     }
-  ]
+  ];
+
+
+  filterUsers() {
+    this.filterArray = this.users.filter((user) =>
+      user.firstName.includes(this.searchInput) || user.lastName.includes(this.searchInput)
+    )
+  }
 }
